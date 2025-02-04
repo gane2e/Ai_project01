9ff656b87f3e549076a306250e9a674a52b08d08
gane2e <b1a409896@naver.com>
2025-02-04 15:36:08 +0900

    rm git

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{user.id}} : {{user.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data(){
+    return {
+      users: [],
+    };
+  },
+  async created(){
+    // const response = await axios.get("http://localhost:8080/users/read");
+    const response = await axios.get("http://127.0.0.1:8000/users");
+    this.users = response.data;
+  }
+}
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+// vue.config.js
+module.exports = {
+  devServer: {
+    port: 8080 
+  }
+}
+
